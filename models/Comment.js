4b996ceb6484175a44e0c2fe8f53d73600c67e02
ADOwen <92840021+ADOwen@users.noreply.github.com>
2022-10-31 14:41:56 -0400

    added validation to Comment model and ReplySchema

--- models/Comment.js
+++ models/Comment.js
@@ -7,13 +7,16 @@ const ReplySchema = new Schema(
     // set custom id to avoid confusion with parent comment _id
     replyId: {
       type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId()
+      default: () => new Types.ObjectId(),
     },
     replyBody: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     writtenBy: {
-      type: String
+      type: String,
+      required: true
     },
     createdAt: {
       type: Date,
@@ -32,10 +35,12 @@ const ReplySchema = new Schema(
 const CommentSchema = new Schema(
   {
     writtenBy: {
-      type: String
+      type: String,
+      required: true,
     },
     commentBody: {
-      type: String
+      type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
